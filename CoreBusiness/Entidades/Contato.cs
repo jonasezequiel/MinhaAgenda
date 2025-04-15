f6c4c64a6d846bd6f5cef89f701cde4b031efd9d
unknown <Senac@TBE015D12.alunos.sc.senac.br>
2025-04-15 20:28:28 -0300

    commit

--- CoreBusiness/Entidades/Contato.cs
+++ CoreBusiness/Entidades/Contato.cs
@@ -7,25 +7,27 @@ namespace CoreBusiness.Entidades
     {
         public Contato()
         {
-            
+
         }
 
-        public Contato(string nome, string? fone, string? email, string endereco)
+        public Contato(string nome, string? fone, string? email, string endereco, string observacao)
         {
             Id = Guid.NewGuid();
             Nome = nome;
             Fone = fone;
             Email = email;
             Endereco = endereco;
+            Observacao = observacao;
         }
 
-        public Contato(Guid id, string nome, string? fone, string? email, string endereco)
+        public Contato(Guid id, string nome, string? fone, string? email, string endereco, string observacao)
         {
             Id = id;
             Nome = nome;
             Fone = fone;
             Email = email;
             Endereco = endereco;
+            Observacao = observacao;
         }
 
         [Required]
@@ -37,5 +39,6 @@ namespace CoreBusiness.Entidades
         [Required]
         public string? Email { get; set; } = string.Empty;
         public string? Endereco { get; set; } = string.Empty;
+        public string? Observacao { get; set; } = string.Empty;
     }
 }
